a733b6bfcdb3891cb852990535202054789adbf7
Oleksandr Kharchenko <dnc.oleksandr.kharchenko@gmail.com>
2023-07-27 22:37:31 +0300

    added migrations

---
+++ migrations/000003_create_otp_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS otp (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(20) NOT NULL,
+    is_used BOOLEAN NOT NULL DEFAULT false,
+    owner_id INT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expired_at TIMESTAMPTZ NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY(owner_id) references users(id)
+);
